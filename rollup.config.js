a51bcea96b4ecc33e547bfc898a0adc1f627b6ea
Luke Edwards <luke.edwards05@gmail.com>
2017-04-13 19:07:38 -0700

    Cleanup Build Chain (#48)
    * condense rollup:* scripts; set multiple targets
    
    * add module entry
    
    * remove mkdirp

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import buble from 'rollup-plugin-buble';
 import flow from 'rollup-plugin-flow';
 
+const pkg = require('./package');
+
 export default {
+	entry: 'src/index.js',
 	useStrict: false,
+	sourceMap: true,
 	plugins: [
-    flow(),
+		flow(),
 		buble()
+	],
+	targets: [
+		{dest: pkg.main, format: 'cjs'},
+		{dest: pkg.module, format: 'es'},
+		{dest: pkg['umd:main'], format: 'umd', moduleName: pkg.name}
 	]
 };
