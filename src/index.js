30331e456350b8cab7677662f933e009a1d75fde
Haroen Viaene <fingebimus@me.com>
2019-04-08 12:18:24 +0200

    replace slice().map by forEach
    This looks functionally identical to me

--- src/index.js
+++ src/index.js
@@ -54,8 +54,8 @@ export default function mitt(all: EventHandlerMap) {
 		 * @memberOf mitt
 		 */
 		emit(type: string, evt: any) {
-			(all[type] || []).slice().map((handler) => { handler(evt); });
-			(all['*'] || []).slice().map((handler) => { handler(type, evt); });
+			(all[type] || []).forEach((handler) => { handler(evt); });
+			(all['*'] || []).forEach((handler) => { handler(type, evt); });
 		}
 	};
 }
