ea0bb11f04c1d635ccb85e07f5f9908af08a82a2
Jack Franklin <jacktfranklin@chromium.org>
2020-05-20 16:24:52 +0100

    fix documentation

--- src/index.ts
+++ src/index.ts
@@ -12,41 +12,13 @@ type EventHandlerMap = {
 };
 
 export interface Emitter {
-	/**
-	 * Register an event handler for the given type.
-	 *
-	 * @param {string} type Type of event to listen for, or `"*"` for all events.
-	 * @param {Handler} handler Function to call in response to the given event.
-	 *
-	 * @memberOf Mitt
-	 */
 	on(type: string, handler: Handler): void;
 	on(type: "*", handler: WildcardHandler): void;
 
-	/**
-	 * Function to call in response to the given event
-	 *
-	 * @param {string} type Type of event to unregister `handler` from, or `"*"`
-	 * @param {Handler} handler Handler function to remove.
-	 *
-	 * @memberOf Mitt
-	 */
 	off(type: string, handler: Handler): void;
 	off(type: "*", handler: WildcardHandler): void;
 
-	/**
-	 * Invoke all handlers for the given type.
-	 * If present, `"*"` handlers are invoked prior to type-matched handlers.
-	 *
-	 * @param {string} type The event type to invoke
-	 * @param {any} [event] An event object, passed to each handler
-	 *
-	 * @memberOf Mitt
-	 */
 	emit(type: string, event?: any): void;
-	/**
-	 * Note: Manually firing "*" events is unsupported.
-	 */
 	emit(type: "*", event?: any): void;
 }
 
